66e99cb4db80001b84d675659373256b749fc680
duc <ducchau.spm@gmail.com>
2022-09-28 11:33:05 +0700

    Add project files.

---
+++ ChuongTrinhDiemDanh_DHThaiNguyen/Classes/GoogleSheet.cs
@@ -0,0 +1,130 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ChuongTrinhDiemDanh_DHThaiNguyen.Classes
+{
+    class GoogleSheet
+    {
+        public string credentialPath;
+        public string spreadSheetID;
+        public string appName;
+
+        public GoogleCredential credential;
+        public SheetsService service;
+
+        string[] Scopes = { SheetsService.Scope.Spreadsheets };
+
+        public bool Begin(string credentialPath, string spreadSheetID, string appName)
+        {
+            try
+            {
+                this.credentialPath = credentialPath;
+                this.spreadSheetID = spreadSheetID;
+                this.appName = appName;
+
+                using (var stream = new FileStream(credentialPath, FileMode.Open, FileAccess.Read))
+                {
+                    credential = GoogleCredential.FromStream(stream).CreateScoped(Scopes);
+                }
+                service = new SheetsService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = appName,
+                });
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ShowMessageBoxException(ex);
+                return false;
+            }
+        }
+
+        public bool WriteData(string range, ValueRange valueRange)
+        {
+            try
+            {
+                var updateRequest = service.Spreadsheets.Values.Update(valueRange, this.spreadSheetID, range);
+                updateRequest.ValueInputOption =
+                    SpreadsheetsResource.
+                    ValuesResource.
+                    UpdateRequest.
+                    ValueInputOptionEnum.
+                    USERENTERED;
+                updateRequest.Execute();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ShowMessageBoxException(ex);
+                return false;
+            }
+        }
+
+        public ValueRange ReadData(string range)
+        {
+            try
+            {
+                SpreadsheetsResource.ValuesResource.GetRequest request =
+                                service.Spreadsheets.Values.Get(this.spreadSheetID, range);
+                ValueRange response = request.Execute();
+                return response;
+            }
+            catch (Exception ex)
+            {
+                ShowMessageBoxException(ex);
+                return null;
+            }
+        }
+
+        public IList<ValueRange> ReadBatchData(string[] ranges)
+        {
+            try
+            {
+                SpreadsheetsResource.ValuesResource.BatchGetRequest request =
+                                service.Spreadsheets.Values.BatchGet(this.spreadSheetID);
+                request.Ranges = ranges;
+                BatchGetValuesResponse response = request.Execute();
+                return response.ValueRanges;
+            }
+            catch (Exception ex)
+            {
+                ShowMessageBoxException(ex);
+                return null;
+            }
+        }
+
+        public bool WriteBatchData(IList<ValueRange> valueRanges, string[] ranges)
+        {
+            try
+            {
+                string valueInputOption = "";
+                BatchUpdateValuesRequest requestBody = new BatchUpdateValuesRequest();
+                requestBody.ValueInputOption = valueInputOption;
+                requestBody.Data = valueRanges;
+
+                SpreadsheetsResource.ValuesResource.BatchUpdateRequest request =
+                    service.Spreadsheets.Values.BatchUpdate(requestBody, this.spreadSheetID);
+
+                BatchUpdateValuesResponse response = request.Execute();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ShowMessageBoxException(ex);
+                return false;
+            }
+        }
+
+
+        public void ShowMessageBoxException(Exception ex)
+        {
+            System.Windows.Forms.MessageBox.Show(ex.Message, ex.GetType().Name);
+        }
+    }
+}
---
+++ ChuongTrinhDiemDanh_DHThaiNguyen/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ChuongTrinhDiemDanh_DHThaiNguyen.Classes;
+using ChuongTrinhDiemDanh_DHThaiNguyen.RJControls;
+
+namespace ChuongTrinhDiemDanh_DHThaiNguyen
+{
+    public partial class MainForm : Form
+    {
+
+        #region global variables
+
+        #endregion
+
+        #region user-defined functions
+
+        #endregion
+
+        #region automatic code generation by VS
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+    }
+}
